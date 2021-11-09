7a843094c0ca017d1c4cf828f6fa99a3b45a5822
AndrewDongminYoo <ydm2790@gmail.com>
2021-11-09 16:51:34 +0900

    First Commit

---
+++ src/main/java/com/sparta/selectshop/SpringPrac02Application.java
@@ -0,0 +1,15 @@
+package com.sparta.selectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpringPrac02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPrac02Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/sparta/selectshop/models/Product.java
@@ -0,0 +1,27 @@
+package com.sparta.selectshop.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Product extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private int lowPrice;
+
+    @Column(nullable = false)
+    private int myPrice;
+}
