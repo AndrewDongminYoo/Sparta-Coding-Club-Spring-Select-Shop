c6eb7a92fd77d397cd061149716da8821fed226f
ydm2790 <ydm2790@gmail.com>
2022-01-05 11:06:12 +0900

    feature :: kakao login

--- src/main/java/com/sparta/selectshop/service/UserService.java
+++ src/main/java/com/sparta/selectshop/service/UserService.java
@@ -4,7 +4,14 @@ import com.sparta.selectshop.dto.SignupRequestDto;
 import com.sparta.selectshop.models.User;
 import com.sparta.selectshop.models.UserRepository;
 import com.sparta.selectshop.models.UserRole;
+import com.sparta.selectshop.security.UserDetailsImpl;
+import com.sparta.selectshop.security.kakao.KakaoOAuth2;
+import com.sparta.selectshop.security.kakao.KakaoUserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,13 +21,18 @@ import java.util.Optional;
 public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final KakaoOAuth2 kakaoOAuth2;
+    private final AuthenticationManager authenticationManager;
     private static final String ADMIN_TOKEN = "AAABnv/xRVklrnYxKZ0aHgTBcXukeZygoC";
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, KakaoOAuth2 kakaoOAuth2, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.kakaoOAuth2 = kakaoOAuth2;
+        this.authenticationManager = authenticationManager;
     }
+
 
     public void registerUser(SignupRequestDto requestDto) {
         String username = requestDto.getUsername();
@@ -44,5 +56,43 @@ public class UserService {
 
         User user = new User(username, password, email, role);
         userRepository.save(user);
+    }
+
+    public void kakaoLogin(String authorizedCode) {
+        // 카카오 OAuth2 를 통해 카카오 사용자 정보 조회
+        KakaoUserInfo userInfo = kakaoOAuth2.getUserInfo(authorizedCode);
+        Long kakaoId = userInfo.getId();
+        String nickname = userInfo.getNickname();
+        String email = userInfo.getEmail();
+
+        // 우리 DB 에서 회원 Id 와 패스워드
+        // 회원 Id = 카카오 nickname
+        // 패스워드 = 카카오 Id + ADMIN TOKEN
+        String password = kakaoId + ADMIN_TOKEN;
+
+        // DB 에 중복된 Kakao Id 가 있는지 확인
+        User kakaoUser = userRepository.findByKakaoId(kakaoId)
+                .orElse(null);
+
+        // 카카오 정보로 회원가입
+        if (kakaoUser == null) {
+            // 패스워드 인코딩
+            User existUser = userRepository.findByEmail(email).orElse(null);
+            if (existUser != null) {
+                existUser.setKakaoId(kakaoId);
+                userRepository.save(existUser);
+            } else {
+                String encodedPassword = passwordEncoder.encode(password);
+                // ROLE = 사용자
+                UserRole role = UserRole.USER;
+                kakaoUser = new User(nickname, encodedPassword, email, role, kakaoId);
+                userRepository.save(kakaoUser);
+            }
+        }
+
+        // 로그인 처리
+        UserDetailsImpl userDetails = new UserDetailsImpl(kakaoUser);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
