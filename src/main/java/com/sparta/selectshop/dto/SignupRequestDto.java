642415007f30fdf57d70af115289513ec56bfac1
ydm2790 <ydm2790@gmail.com>
2022-01-05 10:16:14 +0900

    feature :: style.css

--- src/main/java/com/sparta/selectshop/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/selectshop/dto/SignupRequestDto.java
@@ -1,10 +1,10 @@
 package com.sparta.selectshop.dto;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
+@Data
 public class SignupRequestDto {
     private String username;
     private String password;
